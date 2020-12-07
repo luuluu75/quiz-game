7964e693c794b2c499ee4db2e4061a100a83e25e
Thu Van <luuluu_1975@yahoo.com.au>
2020-12-07 18:10:39 +1100

    first draft of code

---
+++ Assets/script.js
@@ -0,0 +1,158 @@
+
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const startButton = document.getElementById('start');
+
+//questions to include in the quiz
+const myQuestions = [
+  {
+    question: "Who invented JavaScript?",
+    answers: {
+      a: "Douglas Crockford",
+      b: "Sheryl Sandberg",
+      c: "Brendan Eich"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "Who invented JavaScript?",
+    answers: {
+      a: "Douglas Crockford",
+      b: "Sheryl Sandberg",
+      c: "Brendan Eich"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "Who invented JavaScript?",
+    answers: {
+      a: "Douglas Crockford",
+      b: "Sheryl Sandberg",
+      c: "Brendan Eich"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "Who invented JavaScript?",
+    answers: {
+      a: "Douglas Crockford",
+      b: "Sheryl Sandberg",
+      c: "Brendan Eich"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "Who invented JavaScript?",
+    answers: {
+      a: "Douglas Crockford",
+      b: "Sheryl Sandberg",
+      c: "Brendan Eich"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "Who invented JavaScript?",
+    answers: {
+      a: "Douglas Crockford",
+      b: "Sheryl Sandberg",
+      c: "Brendan Eich"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "Who invented JavaScript?",
+    answers: {
+      a: "Douglas Crockford",
+      b: "Sheryl Sandberg",
+      c: "Brendan Eich"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "Who invented JavaScript?",
+    answers: {
+      a: "Douglas Crockford",
+      b: "Sheryl Sandberg",
+      c: "Brendan Eich"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "Who invented JavaScript?",
+    answers: {
+      a: "Douglas Crockford",
+      b: "Sheryl Sandberg",
+      c: "Brendan Eich"
+    },
+    correctAnswer: "c"
+  },
+  {
+    question: "Who invented JavaScript?",
+    answers: {
+      a: "Douglas Crockford",
+      b: "Sheryl Sandberg",
+      c: "Brendan Eich"
+    },
+    correctAnswer: "c"
+  },
+];
+
+buildQuiz();
+
+function buildQuiz()
+//set time on click, countdown from start
+var timeLeft = 120;
+{startButton.addEventListener('click', quiz);}
+
+
+var timeInterval = setInterval(function() {
+  timerEl.textContent = timeLeft + " seconds remaining";
+  timeLeft--;
+
+if (timeLeft === 0) {
+  timerEl.textContent = "";
+  buildQuiz();
+  clearInterval(timeInterval);
+//results must display at once game over. 
+
+
+function showResults()
+
+{
+
+}
+
+
+
+// display quiz right away
+
+
+// on submit, show results
+
+function prepareRead() {
+  var timeLeft = 120;
+
+
+
+
+    }
+
+  }, 1000);
+}
+
+
+function speedRead() {
+  mainEl.append(bodyEl);
+
+  var poemInterval = setInterval(function() {
+    if (words[i] === undefined) {
+      clearInterval(poemInterval);
+    } else {
+      mainEl.textContent = words[i];
+      i++;
+    }
+
+  }, millisecondsPerWord);
+}
+
+prepareRead();
